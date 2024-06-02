a653e48fa65a4f8d2ce984c7584b1295a7cccf0a
Darkzero7 <chaiyanituporn@gmail.com>
2024-06-03 00:01:48 +0700

    js lab 106

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-// write code here
+function ask(question, yes, no) {
+  if (confirm(question)) yes();
+  else no();
+}
+
+function showOk() {
+  alert("I'm agree with you ?");
+}
+
+function showcancel() {
+  alert("whyyyyyyy !");
+}
+
+ask("DO you agree?", showOk, showcancel);
